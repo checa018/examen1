2bc1e9e29ef74f4e9f0b8e0e03db56b67c273897
checa018 <checamarco649@gmail.com>
2022-09-09 10:35:51 -0400

    examen 1

---
+++ Documentos.java
@@ -0,0 +1,11 @@
+ public class Documentos/ {
+     String url;
+     Tema tema ;
+    Index Index;
+     String titulo;
+     {
+    }
+    public  Documentos ( String  url , Tema tema , Index Index ,String titulo ) {
+       String_ url = url;
+        String_ Tema = Tema;
+        String_ Index = Index;
---
+++ index.java
@@ -0,0 +1,14 @@
+public class index 
+¨{
+    Boolean hash;
+    Integer tamanio;
+    Date fecha;
+    Documentos documento;
+    public  Documentos ( String  url , Tema tema , Index Index ,String titulo ) {
+       String_ url = url;
+        String_ Tema = Tema;
+        String_ Index = Index;
+
+
+
+}
